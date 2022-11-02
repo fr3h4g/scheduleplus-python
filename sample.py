21876527be4467a7119d0d1e36d2949e678323b2
Fredrik Haglund <fredrik@petitcircuitlab.com>
2022-11-02 23:08:02 +0100

    added sample

---
+++ sample.py
@@ -0,0 +1,15 @@
+import time
+from scheduleplus.schedule import Scheduler
+
+
+def func():
+    print("Working...")
+
+
+schedule = Scheduler()
+
+schedule.cron("*/5 * * * *").do(func)
+
+while True:
+    schedule.run_jobs()
+    time.sleep(1)
